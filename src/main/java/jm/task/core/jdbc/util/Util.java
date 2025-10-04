ec5a415e7e703baa0f87c5a285736315777ab296
Vladimir Ligai <afk1minpql@gmail.com>
2025-10-04 14:12:52 +0900

    Add getConnection to Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // set up a database connection
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
+    }
 }
